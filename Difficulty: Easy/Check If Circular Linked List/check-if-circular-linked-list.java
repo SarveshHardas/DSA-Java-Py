c0b8dccd1caa2e3b0fb8450c156d6a8cd53790e4
Sarvesh Hardas <86554291+SarveshHardas@users.noreply.github.com>
2026-06-30 10:54:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check If Circular Linked List/check-if-circular-linked-list.java
@@ -0,0 +1,30 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}
+*/
+class Solution {
+    boolean isCircular(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+}
